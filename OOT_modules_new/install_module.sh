652007e5800edbceb9894adeed24b5a006870fef
iNETS | RWTH Aachen University <115540227+inets-rwth@users.noreply.github.com>
2024-11-18 19:24:25 +0100

    Update install_module.sh

--- OOT_modules_new/install_module.sh
+++ OOT_modules_new/install_module.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd
-cd /home/inets/Workspace/new_setup/OOT_modules_new/gr-iNETS_SIVERSControl
+cd ~/GNURadio-mmWave/OOT_modules_new/gr-iNETS_SIVERSControl
 sudo rm -r build/
 mkdir build
 cd build
 cmake ..
-make -j5
+make -j4
 sudo make install
 sudo ldconfig
